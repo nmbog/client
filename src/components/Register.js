8a62efc48ca3b4437a4a4b855ba16c553ff38c49
Nicole Bogart <nicolemariebogart@gmail.com>
2023-11-29 18:19:24 -0700

    Updated Register.js, index.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,9 +6,35 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+
+  const signUp = () => {
+    fetch("http://localhost:4000/api/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+        username,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error_message) {
+          alert(data.error_message);
+        } else {
+          alert("Account created successfully!");
+          navigate("/");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ username, email, password });
+    signUp();
     setEmail("");
     setUsername("");
     setPassword("");
